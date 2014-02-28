ae874b46c1a8652b9e8a3a09a7ad6421547a9b1e
Geoff_McGinnis <mcginnis.1930@osu.edu>
2014-02-28 14:50:48 -0500

    Created new C# project, getting rid of excess from other project.

--- SQLVisualBuilder/Properties/AssemblyInfo.cs
+++ SQLVisualBuilder/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QueryBuilder")]
+[assembly: AssemblyTitle("SQLVisualBuilder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("QueryBuilder")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SQLVisualBuilder")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("99f94076-3d4c-494d-b454-d1e2a5eb19dd")]
+[assembly: Guid("7aa11408-bd0b-4596-9bf7-47ef13d4fe96")]
 
 // Version information for an assembly consists of the following four values:
 //
