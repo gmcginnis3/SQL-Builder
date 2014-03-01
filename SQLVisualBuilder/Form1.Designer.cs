f136c70424a1ed814472f325799660a93b71ba83
Geoff_McGinnis <mcginnis.1930@osu.edu>
2014-03-01 12:52:37 -0500

    Updated form to allow user to generate a query. Also formatting fixes.

--- SQLVisualBuilder/Form1.Designer.cs
+++ SQLVisualBuilder/Form1.Designer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 using System.Windows.Forms;
 using System.Collections;
+using System.Drawing;
 namespace SQLVisualBuilder
 {
     partial class Form1
@@ -32,9 +33,17 @@ namespace SQLVisualBuilder
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -47,33 +56,95 @@ namespace SQLVisualBuilder
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // tabControl1
-            // 
-            this.tabControl1.Location = new System.Drawing.Point(0, 27);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1006, 365);
-            this.tabControl1.TabIndex = 2;
-            // 
             // fileToolStripMenuItem
             // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Location = new System.Drawing.Point(3, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(812, 343);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 27);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(1006, 352);
+            this.splitContainer1.SplitterDistance = 187;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(0, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Query!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(0, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 278);
+            this.textBox1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1006, 391);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(1006, 369);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "SQL Visualizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,24 +152,13 @@ namespace SQLVisualBuilder
 
         #endregion
 
-
-        public void setData(ArrayList items)
-        {
-            foreach (string table in items)
-            {
-                TabPage tp = new TabPage(table);
-                tabControl1.TabPages.Add(tp);
-                DataGridView dgv = new DataGridView();
-                dgv.Dock = DockStyle.Fill;
-                dgv.AutoGenerateColumns = true;
-                dgv.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
-                dgv.DataSource = Program.Query("SELECT * FROM " + table);
-                dgv.Parent = tp;
-            }
-        }
         private MenuStrip menuStrip1;
         private TabControl tabControl1;
         private ToolStripMenuItem fileToolStripMenuItem;
+        private SplitContainer splitContainer1;
+        private Button button1;
+        private ToolStripMenuItem quitToolStripMenuItem;
+        private TextBox textBox1;
     }
 }
 
--- SQLVisualBuilder/Form1.cs
+++ SQLVisualBuilder/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace SQLVisualBuilder
 {
@@ -20,5 +21,77 @@ namespace SQLVisualBuilder
         {
 
         }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TabPage page = tabControl1.SelectedTab;
+            if (!page.HasChildren)
+                return;
+            DataGridView dgv = page.Controls.Find("dataGrid", true)[0] as DataGridView;
+            DataGridViewSelectedColumnCollection selected = dgv.SelectedColumns;
+            textBox1.Text = selected.Count.ToString();
+
+            QueryBuilder builder = new QueryBuilder();
+            builder.QueryType = QueryTypes.Select;
+            builder.AddTable(page.Text);
+            if (selected.Count > 0)
+            {
+                foreach (DataGridViewColumn col in selected)
+                {
+                    builder.AddColumn(col.HeaderText);
+                }
+            }
+            else
+            {
+                builder.AddColumn("*");
+            }
+
+            textBox1.Text += builder.ToString();
+        }
+
+        public void setData(ArrayList items)
+        {
+            //tabControl1.Parent = splitContainer1.Panel2;
+            foreach (string table in items)
+            {
+                TabPage tp = new TabPage(table);
+                tabControl1.TabPages.Add(tp);
+                DataGridView dgv = new DataGridView();
+                dgv.Name = "dataGrid";
+                dgv.Anchor = (AnchorStyles.Bottom | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Left); ;
+                dgv.Dock = DockStyle.Fill;
+                dgv.AutoGenerateColumns = true;
+                dgv.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+                dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dgv.DataSource = Program.Query("SELECT * FROM " + table);
+                dgv.Parent = tp;
+                /*foreach (DataGridViewColumn column in dgv.Columns)
+                {
+                    column.SortMode = DataGridViewColumnSortMode.Programmatic;
+                }
+                dgv.SelectionMode = DataGridViewSelectionMode.ColumnHeaderSelect;*/
+            }
+        }
+
     }
 }
